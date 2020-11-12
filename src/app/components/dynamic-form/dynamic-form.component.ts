f4077b28649e6df3a827b6f41e7ccedffe31a7d6
George Dimulski <geroge.dimulski@gmail.com>
2020-11-12 02:27:09 +0200

    Change dynamic form based on radio buttons

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -10,26 +10,46 @@ export class DynamicFormComponent implements OnInit {
 
   public dynamicForm: FormGroup;
   public sentStatuses = ['Already sent', 'Not sent yet'];
+  public urgencyState = ['Urgent', 'Not urgent'];
 
   constructor() { }
 
   ngOnInit(): void {
     this.dynamicForm = new FormGroup({
-      'sentStatus': new FormControl(null, Validators.required),
-      'alreadySent': new FormGroup({
-        'document': new FormControl(null, Validators.required),
-        'sendDate': new FormControl(null, Validators.required),
-        'comment': new FormControl(null)
-      }),
-      'notSentYet': new FormGroup({
-        'passportNumber': new FormControl(null, Validators.required)
-      }),
-      'urgentStatus': new FormControl(null, Validators.required),
-      'deadline': new FormControl(null, Validators.required) //
-    })
+      'sentStatus': new FormControl(null),
+      'urgencyState': new FormControl(null, Validators.required)
+    });
+
+    this.dynamicForm.get('sentStatus').valueChanges.subscribe((value) => {
+      if (value == 'Already sent') {
+        this.dynamicForm.removeControl('notSentYet');
+        this.dynamicForm.addControl('alreadySent', new FormGroup({
+          'document': new FormControl(null, Validators.required),
+          'sendDate': new FormControl(null, Validators.required),
+          'comment': new FormControl(null)
+        }));
+      } else {
+        this.dynamicForm.removeControl('alreadySent');
+        this.dynamicForm.addControl('notSentYet', new FormGroup({
+          'passportNumber': new FormControl(null, Validators.required)
+        }));
+      }
+    });
+
+    this.dynamicForm.get('urgencyState').valueChanges.subscribe((value) => {
+      if (value == 'Urgent') {
+        this.dynamicForm.addControl('deadline', new FormControl(null, Validators.required));
+      } else {
+        this.dynamicForm.removeControl('deadline');
+      }
+    });
   }
 
-  public onSubmit(): void {
+  public onSubmit(form: NgForm): void {
+    console.log(form)
+  }
+
+  print() {
     console.log(this.dynamicForm)
   }
 }
